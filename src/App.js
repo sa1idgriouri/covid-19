5f4957479eb2bb4e5fe04b6096af4b2e94d32783
Said Griouri <said@abacall.local.com>
2022-03-14 20:32:18 +0100

    static covid 19 project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Card, Chart, CountryPiker } from "./compenents"
+import image from './images/image.png';
+
+import style from "./App.module.css"
+import { fetchData } from "./api"
+class App extends Component {
+  state = {
+    data: [],
+    country: '',
+  }
+  async componentDidMount() {
+
+
+    const data = await fetchData()
+
+    this.setState({ data: data })
+
+  }
+
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
+  }
+
+  render() {
+    const { data, country } = this.state;
+    return (
+      <div className={style.container}>
+        <img className={style.image} src={image} alt="COVID-19" />
+        <Card data={data} />
+        <CountryPiker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/compenents/index.js
@@ -0,0 +1,3 @@
+export { default as Card } from "./cards/Cards"
+export { default as Chart } from "./chart/Chart.jsx"
+export { default as CountryPiker } from "./conturyPiker/CountryPiker.jsx"
